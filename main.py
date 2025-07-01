47f48a783d056a0973cbb14cf7d6d9a27161ae8f
Lachlan Rixon <lrixon252@gmail.com>
2025-07-01 19:51:58 +1000

    updated dic_sort, updated main()

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from stats import word_num
 from stats import char_count
+from stats import dic_sort
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    total = word_num(get_book_text("books/frankenstein.txt"))
-    print(f"{total} words found in the document")
-    print(char_count(get_book_text("books/frankenstein.txt")))
+    get_book = get_book_text("books/frankenstein.txt")
 
+    print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt
+----------- Word Count ----------
+Found {word_num(get_book)} total words
+--------- Character Count -------""")
+    for dic in dic_sort(char_count(get_book)):
+        print(f"{dic["char"]}: {dic["num"]}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -3,10 +3,21 @@ def word_num(book):
 
 def char_count(book):
     char_dic = {}
-
     for char in book.lower():
         if char in char_dic:
             char_dic[f'{char}'] += 1
         else:
             char_dic[f'{char}'] = 1
     return char_dic
+
+def sort_on(c):
+    return c["num"]
+
+def dic_sort(char_dic):
+    char_dic_list = []
+    for char in char_dic:
+        if char.isalpha():
+            char_dic_list.append({"char":char, "num":char_dic[char]})
+    char_dic_list.sort(reverse=True, key=sort_on)
+    return char_dic_list
+    
