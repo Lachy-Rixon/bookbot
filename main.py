665a01ada17ba199956d9360de4e4321cf3c95d5
Lachlan Rixon <lachlan_rixon@icloud.com>
2025-07-01 20:50:52 +1000

    finished bookbot

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from stats import word_num
 from stats import char_count
 from stats import dic_sort
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    get_book = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    get_book = get_book_text(sys.argv[1])
 
     print(f"""============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt
+Analyzing book found at {sys.argv[1]}
 ----------- Word Count ----------
 Found {word_num(get_book)} total words
 --------- Character Count -------""")
